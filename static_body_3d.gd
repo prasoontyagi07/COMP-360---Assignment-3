252f7cfd913ecad94b2ac58e3da327beaf119e3b
Mihir <mihir.badhan@student.ufv.ca>
2025-12-13 02:06:52 -0800

    added real crate targets, color variants, added smashtarget scene which will be used later to spawn crates

--- static_body_3d.gd
+++ static_body_3d.gd
@@ -1,26 +1,26 @@
 extends StaticBody3D
 
 @export var fade_time := 0.25
-var _smashed := false
-@onready var mesh: MeshInstance3D = _find_mesh(self)
 
-func _find_mesh(n: Node) -> MeshInstance3D:
-	for c in n.get_children():
-		if c is MeshInstance3D:
-			return c
-		var m := _find_mesh(c)
-		if m != null:
-			return m
-	return null
+var _smashed := false
+@onready var _meshes: Array[GeometryInstance3D] = []
+
+func _ready() -> void:
+	# Grab every visible 3D geometry 
+	for n in find_children("*", "GeometryInstance3D", true, false):
+		_meshes.append(n)
 
 func smash() -> void:
 	if _smashed:
 		return
 	_smashed = true
 
-	if mesh:
+	# Fade every mesh in parallel
+	if _meshes.size() > 0:
 		var t := create_tween()
-		t.tween_property(mesh, "transparency", 1.0, fade_time)
+		t.set_parallel(true)
+		for m in _meshes:
+			t.tween_property(m, "transparency", 1.0, fade_time)
 		await t.finished
 
 	queue_free()
