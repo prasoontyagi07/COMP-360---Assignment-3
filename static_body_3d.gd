1d23f6776d85f7d23b7e762964ce9b25994de170
Mihir <mihir.badhan@student.ufv.ca>
2025-12-13 00:21:32 -0800

    Added script to targets so they fade instead of just delete

--- static_body_3d.gd
+++ static_body_3d.gd
@@ -1,17 +1,23 @@
 extends StaticBody3D
 
 @export var fade_time := 0.25
-
-@onready var mesh: MeshInstance3D = $MeshInstance3D
-
 var _smashed := false
+@onready var mesh: MeshInstance3D = _find_mesh(self)
+
+func _find_mesh(n: Node) -> MeshInstance3D:
+	for c in n.get_children():
+		if c is MeshInstance3D:
+			return c
+		var m := _find_mesh(c)
+		if m != null:
+			return m
+	return null
 
 func smash() -> void:
 	if _smashed:
 		return
 	_smashed = true
 
-	# Quick fade (no material setup needed)
 	if mesh:
 		var t := create_tween()
 		t.tween_property(mesh, "transparency", 1.0, fade_time)
