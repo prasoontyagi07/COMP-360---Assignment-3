f237f3915a4488920d5a9e9d9231ff4edc1b5267
Prasoon Tyagi <prasoontyagi123@gmail.com>
2025-12-14 18:53:58 -0800

    Add particle system for smash effect

--- static_body_3d.gd
+++ static_body_3d.gd
@@ -29,6 +29,7 @@ func smash() -> void:
 		robot_node.reset_smash()
 	# -------------------------------------------------------------
 
+	_spawn_smash_particles(global_position + Vector3(0, 0.25, 0))
 	_spawn_coins()
 
 	# fade then remove
@@ -66,3 +67,43 @@ func _spawn_coins() -> void:
 		var pop := coin.create_tween()
 		pop.tween_property(coin, "global_position", coin.global_position + Vector3(0, 0.4, 0), 0.12)
 		pop.tween_property(coin, "global_position", coin.global_position, 0.18)
+func _spawn_smash_particles(at_pos: Vector3) -> void:
+	var p := GPUParticles3D.new()
+	p.one_shot = true
+	p.emitting = false
+	p.amount = 45
+	p.lifetime = 0.35
+	p.explosiveness = 1.0
+	p.randomness = 0.8
+
+	# IMPORTANT: particles need a draw mesh or you won't see anything
+	var m := SphereMesh.new()
+	m.radius = 0.03
+	m.height = 0.06
+	p.draw_pass_1 = m
+
+	var mat := ParticleProcessMaterial.new()
+	mat.gravity = Vector3(0, -12.0, 0)
+	mat.direction = Vector3(0, 1, 0)
+	mat.spread = 180.0
+	mat.initial_velocity_min = 2.0
+	mat.initial_velocity_max = 6.0
+	mat.angular_velocity_min = -8.0
+	mat.angular_velocity_max = 8.0
+	mat.damping_min = 1.0
+	mat.damping_max = 3.0
+	mat.scale_min = 0.4
+	mat.scale_max = 1.0
+
+	p.process_material = mat
+
+	get_parent().add_child(p)
+	p.global_position = at_pos
+
+	# fire once, then clean up
+	p.restart()
+	p.emitting = true
+
+	await get_tree().create_timer(p.lifetime + 0.2).timeout
+	if is_instance_valid(p):
+		p.queue_free()
